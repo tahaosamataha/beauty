ca9cd54d9245ac2f9069b38e544f911402f1c47a
Taha Osama <taha.mediabuyer@gmail.com>
2025-08-16 23:33:03 +0000

    New Changes

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const input = document.getElementById('custom-marquee-input');
   const marqueeText = document.getElementById('custom-marquee-text');
+
   if (!input || !marqueeText) return;
 
   input.addEventListener('input', () => {
-    marqueeText.textContent = input.value.trim() || 'مرحباً بكم في Beauty Theme!';
+    marqueeText.textContent = input.value || 'مرحباً بكم في Beauty Theme!';
   });
 });
